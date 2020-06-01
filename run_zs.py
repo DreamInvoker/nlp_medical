84659c083430c8db94d093db238944369d09906c
zengshuang <631710518@qq.com>
2020-06-01 15:33:54 +0800

    add some trick

--- run_zs.py
+++ run_zs.py
@@ -5,11 +5,13 @@ import torch
 from torch import nn
 from torch import optim
 from torch.utils.data import DataLoader
-
+from tqdm import tqdm
 from config import *
 from dataset_zs import MedicalExtractionDatasetForSubjectAndBody
 from model import MedicalExtractionModel
-from utils import get_cuda, logging, print_params
+from utils import get_cuda, logging, print_params, EarlyStopping
+
+# TODO 监控验证集上的评价指标比如F1或者jaccard score
 
 
 def train(opt):
@@ -58,6 +60,7 @@ def train(opt):
 
     global_step = 0
     total_loss = 0
+    es = EarlyStopping(patience=5, mode="min", criterion='val loss')
     for epoch in range(start_epoch, total_epochs + 1):
         start_time = time.time()
         train_dl = DataLoader(train_ds,
@@ -66,7 +69,8 @@ def train(opt):
                               num_workers=1
                               )
         model.train()
-        for batch in train_dl:
+        tk0 = tqdm(train_dl, total=len(train_dl))
+        for batch in tk0:
             optimizer.zero_grad()
             subject_target_ids = batch['subject_target_ids']
             body_target_ids = batch['body_target_ids']
@@ -79,45 +83,64 @@ def train(opt):
             loss = criterion(subject_logits, subject_target_ids) + criterion(body_logits, body_target_ids)
             loss.backward()
             optimizer.step()
-
+            tk0.set_postfix(train_loss='{:5.3f} / 1000'.format(1000 * loss.item()),
+                            epoch='{:2d}'.format(epoch),
+                            )
+            '''
             global_step += 1
             total_loss += loss.item()
             if global_step % log_step == 0:
                 cur_loss = total_loss / log_step
                 elapsed = time.time() - start_time
                 logging(
-                    '| epoch {:2d} | step {:4d} |  ms/b {:5.2f} | train loss {:5.3f} '.format(
+                    '| epoch {:2d} | step {:4d} |  ms/b {:5.2f} | train loss (x1000) {:5.3f} '.format(
                         epoch, global_step, elapsed * 1000 / log_step, cur_loss * 1000))
                 total_loss = 0
                 start_time = time.time()
+            '''
+        # TODO 如果eval时间太久，可能不太需要每个epoch都验证一次
+        # if epoch % opt.test_epoch == 0:
+        model.eval()
+        val_loss = 0.0
+        with torch.no_grad():
+            for batch in dev_dl:
+                subject_target_ids = batch['subject_target_ids']
+                body_target_ids = batch['body_target_ids']
 
-        if epoch % opt.test_epoch == 0:
-            model.eval()
-            with torch.no_grad():
-                for batch in dev_dl:
-                    subject_target_ids = batch['subject_target_ids']
-                    body_target_ids = batch['body_target_ids']
+                subject_logits, body_logits = model(
+                    input_ids=batch['input_ids'],
+                    attention_mask=batch['mask'],
+                    token_type_ids=batch['token_type_ids']
+                )
+                loss = criterion(subject_logits, subject_target_ids) + criterion(body_logits, body_target_ids)
+                val_loss += loss.item() * subject_target_ids.shape[0]
+        avg_val_loss = val_loss * 1000 / len(dev_ds)
+        logging('val loss per example: {:5.3f} / 1000'.format(avg_val_loss))
 
-                    subject_logits, body_logits = model(
-                        input_ids=batch['input_ids'],
-                        attention_mask=batch['mask'],
-                        token_type_ids=batch['token_type_ids']
-                    )
-                    loss = criterion(subject_logits, subject_target_ids) + criterion(body_logits, body_target_ids)
-                    logging('val loss: {}'.format(loss.item()))
+        # 保留最佳模型方便evaluation
+        save_model_path = os.path.join(checkpoint_dir, model_name + '_best.pt'.format(epoch))
+        es(avg_val_loss, model, model_path=save_model_path, epoch=epoch, learning_rate=learning_rate)
+        if es.early_stop:
+            print("Early stopping")
+            break
 
-        # save model
-        # TODO 可以改成只save在dev上最佳的模型
+        # 保存epoch的模型方便断点续训
         if epoch % opt.save_model_freq == 0:
-            path = os.path.join(checkpoint_dir, model_name + '_{}.pt'.format(epoch))
+            save_model_path = os.path.join(checkpoint_dir, model_name + '_{}.pt'.format(epoch))
             torch.save({
                 'epoch': epoch,
                 'learning_rate': learning_rate,
                 'checkpoint': model.state_dict()
-            }, path)
+            }, save_model_path)
 
 
 if __name__ == '__main__':
+    from multiprocessing import set_start_method
+
+    try:
+        set_start_method('spawn')
+    except RuntimeError:
+        pass
     print('processId:', os.getpid())
     print('prarent processId:', os.getppid())
     opt = get_opt()
--- train.sh
+++ train.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 export CUDA_VISIBLE_DEVICE=$1
+lr=3e-5
 batch_size=16
 epochs=1000
 model_name=baseline
-
+save_model_freq=3
 nohup python -u train_zs.py \
+--lr ${lr} \
 --batch_size ${batch_size} \
 --epochs ${epochs} \
 --log_step 50 \
+--save_model_freq ${save_model_freq} \
 >> logs/train_${model_name}.log \
 2>&1 &
 
